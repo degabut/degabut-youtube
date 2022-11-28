a8080cc33af0b546410b58cc19b6f078f298f9aa
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-11-29 00:05:10 +0700

    fix host

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
-  await app.listen(8080);
+  app.listen(8080, "0.0.0.0");
 }
 bootstrap();
