fd83354199685f703b15ee8800f02ab9b9ab64e7
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2022-11-30 18:25:53 +0700

    remove unused service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,12 @@ import { ConfigModule } from "@nestjs/config";
 import { YoutubeModule } from "@youtube/youtube.module";
 
 import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { JwtAuthConfigModule } from "./config";
 import { JwtAuthProvider } from "./guards";
 
 @Module({
   imports: [ConfigModule, JwtAuthConfigModule, YoutubeModule],
   controllers: [AppController],
-  providers: [AppService, JwtAuthProvider],
+  providers: [JwtAuthProvider],
 })
 export class AppModule {}
