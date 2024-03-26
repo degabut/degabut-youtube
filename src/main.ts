c025cebea45a4b7eb0f3cd553704a8351e6fe25a
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2024-03-26 10:11:04 +0700

    enable cors on dev

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { Logger } from "nestjs-pino";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new FastifyAdapter(), {});
+  const app = await NestFactory.create(AppModule, new FastifyAdapter(), {
+    cors: process.env.NODE_ENV === "development",
+  });
 
   app.useLogger(app.get(Logger));
   app.listen(+(process.env.PORT || 8080), "0.0.0.0");
